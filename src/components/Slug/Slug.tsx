2a963e2a132d74d5079e27e3cb6fb883dbc3604e
Adrian Garcia Rios <agarciar@GMU.EDU>
2023-12-25 21:59:50 -0500

    Changes

--- src/components/Slug/Slug.tsx
+++ src/components/Slug/Slug.tsx
@@ -18,7 +18,7 @@ export default function Slug(slug:SlugType){
         <div className='sm:flex flex-row justify-between'>
           <div className='sm:grid md:grid-flow-col'>
             <Image src={singlePost.data?.img} alt={singlePost.data?.title} width={200} height={200} priority />
-            <div className='flex flex-col'>
+            <div className='flex flex-col pl-1'>
               <p className='text-white text-5xl'>{singlePost.data?.title}</p>
               <span className='text-white prose-h1:text-white prose prose-h2:text-white prose-h3:text-white' dangerouslySetInnerHTML={{__html: singlePost.data?.desc}}/>
             </div>
